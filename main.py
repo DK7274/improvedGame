52a80a60139cd5913b5490e32b2e6ac903cb4496
Darcy Keys <dkeys8722@gmail.com>
2022-07-26 11:10:04 +1000

    finished base, need to lessen jump height

--- main.py
+++ main.py
@@ -20,6 +20,64 @@ bg = pygame.image.load("Images/background.png")
 bg = pygame.transform.scale(bg,(winx,winy))
 
 ship = pygame.image.load("Images/mario.png")
-ship = pygame.transfom.scale(ship,(width,height))
+ship = pygame.transform.scale(ship,(width,height))
 
 ###positioning the rect in the middle###
+
+###position the rect in the middle and at the bottom
+x = 250 # position of the rect
+y = 250 # position on y of rect
+
+vel = 10 #velocity or attack value
+jumpMax = 10 #set number of iterations for jump maths
+
+isJump = False #routine for jump
+jumpCount = jumpMax
+
+###Begin of defining functions###
+
+#screen refresh function#
+def redrawGameWindow():
+    win.blit(bg, (0,0))
+    win.blit(ship,(x,y))
+    pygame.display.update()
+
+run = True
+
+while run:
+    pygame.time.delay(60) #setting game run speed
+
+    for event in pygame.event.get(): #listen for events
+        if event.type == pygame.QUIT: #quit
+            run = False
+
+    keys = pygame.key.get_pressed() #variable for keypress
+
+    if keys[pygame.K_LEFT]: #add the   fucking x > vel here later dumbass
+        x -= vel
+        # if pressing left and position of rect is greater than velocity - movement speed in pixels
+    if keys[pygame.K_RIGHT] and x < 500 - vel - width:
+        x += vel
+
+    if not(isJump):
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+
+        if keys[pygame.K_DOWN] and y < 500 - height - vel:
+            y += vel
+
+        if keys[pygame.K_SPACE]:
+            isJump = True
+
+    else:
+        if jumpCount >= -jumpMax:
+            y -= (jumpCount * abs(jumpCount))
+            print("jump grav", y) #show the meth
+            jumpCount -= 1
+            print("rect position", (jumpCount - y))
+        else:
+            jumpCount = jumpMax
+            isJump = False
+    #win.fill((0,0,0))
+    redrawGameWindow()
+pygame.quit()
